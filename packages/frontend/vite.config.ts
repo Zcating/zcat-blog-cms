a37b92d04e640b068b74c37786dcb9e638a2ce96
zcating <zcating@qq.com>
2025-08-21 14:10:57 +0800

    fix: 将前端代理目标端口从3000更改为9090

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -8,13 +8,13 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000/api',
+        target: 'http://localhost:9090/api',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
       '/static': {
-        target: 'http://localhost:3000/',
+        target: 'http://localhost:9090/',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/static/, ''),
