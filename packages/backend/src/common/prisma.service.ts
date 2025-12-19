0582c059c4d1553737df56adcfda2eb46627de56
zcating <zcating@qq.com>
2025-12-19 23:37:59 +0800

    refactor(prisma): 更新prisma配置和服务以使用新的客户端路径
    将tsconfig中的prisma路径更新为指向客户端目录，并重构PrismaService以使用ConfigService获取数据库配置

--- packages/backend/src/common/prisma.service.ts
+++ packages/backend/src/common/prisma.service.ts
@@ -1,18 +1,26 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 // import { PrismaMariadb } from '@prisma/adapter-mariadb';
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
-import { PrismaClient } from 'generated/prisma';
+import { PrismaClient } from 'generated/prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
-    const connectionString = process.env.DATABASE_URL;
+  constructor(private configService: ConfigService) {
+    const adapter = new PrismaMariaDb({
+      host: configService.get('DATABASE_HOST'),
+      port: configService.get('DATABASE_PORT'),
+      user: configService.get('DATABASE_USER'),
+      password: configService.get('DATABASE_PASSWORD'),
+      database: configService.get('DATABASE_NAME'),
+      connectionLimit: 5,
+    });
 
-    const adapter = new PrismaMariaDb(connectionString);
-
-    super({ adapter });
+    super({
+      adapter,
+    });
   }
 
   async onModuleInit() {
