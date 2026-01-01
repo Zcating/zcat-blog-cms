f8fce078886f267b1ddbdddc7d3279c8d7901b3a
zcating <zcating@qq.com>
2026-01-01 20:17:33 +0800

    perf(oss): 添加私有URL缓存以提高性能
    引入缓存机制来存储私有下载URL，减少重复生成URL的开销。缓存最大容量为2000条，过期时间为1小时。当缓存达到上限时，会自动清理过期条目。

--- packages/backend/src/common/oss.service.ts
+++ packages/backend/src/common/oss.service.ts
@@ -3,11 +3,16 @@ import { ConfigService } from '@nestjs/config';
 
 import * as qiniu from 'qiniu';
 
+// 缓存过期时间 1 小时
+const TTL = 3600;
+const MAX_CACHE_SIZE = 2000;
+
 @Injectable()
 export class OssService {
   private readonly bucketManager: qiniu.rs.BucketManager;
   private readonly domain: string;
   private readonly bucketName: string;
+  private readonly privateUrlCache = new Cache<string>(MAX_CACHE_SIZE, TTL);
 
   constructor(private configService: ConfigService) {
     const accessKey = this.configService.get<string>('OSS_ACCESS_KEY') ?? '';
@@ -24,13 +29,22 @@ export class OssService {
     if (!filename) {
       return '';
     }
-    // 一小时有效期
-    const deadline = Math.floor(Date.now() / 1000) + 3600;
-    return this.bucketManager.privateDownloadUrl(
+    const now = Math.floor(Date.now() / 1000);
+    const cached = this.privateUrlCache.get(filename);
+    if (cached) {
+      return cached;
+    }
+
+    const deadline = now + TTL;
+    const url = this.bucketManager.privateDownloadUrl(
       this.domain,
       filename,
       deadline,
     );
+
+    this.privateUrlCache.set(filename, url);
+
+    return url;
   }
 
   async deleteFile(filename: string) {
@@ -40,5 +54,39 @@ export class OssService {
     } catch {
       return false;
     }
+  }
+}
+
+class Cache<T> {
+  private readonly cache = new Map<string, { value: T; deadline: number }>();
+
+  constructor(
+    private readonly maxSize: number = 1000,
+    private readonly ttl: number = 60 * 1000,
+  ) {}
+
+  get(key: string): T | undefined {
+    const now = this.getCurrentSecond();
+    const cached = this.cache.get(key);
+    if (cached && now < cached.deadline + 30) {
+      return cached.value;
+    }
+    return undefined;
+  }
+
+  set(key: string, value: T) {
+    const now = this.getCurrentSecond();
+    this.cache.set(key, { value, deadline: now + this.ttl });
+    if (this.cache.size > this.maxSize) {
+      for (const [key, value] of this.cache) {
+        if (now >= value.deadline + 30) {
+          this.cache.delete(key);
+        }
+      }
+    }
+  }
+
+  private getCurrentSecond() {
+    return Math.floor(Date.now() / 1000);
   }
 }
