660d53daf49b96d82bd7b150bc053b31627de706
zcating <zcating@qq.com>
2026-01-17 10:33:51 +0800

    feat(z-form): 添加表单提交时的禁用状态和样式优化
    为表单组件添加提交时的禁用状态，防止重复提交。同时优化表单布局样式，使用cn工具类合并className，并确保表单宽度占满容器。

--- packages/ui/src/design/z-form/z-form.tsx
+++ packages/ui/src/design/z-form/z-form.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { cn } from '@zcat/ui/shadcn/lib/utils';
 import { Form } from '@zcat/ui/shadcn/ui/form';
 
 import { ZFormContext } from './z-form-context';
@@ -15,11 +16,18 @@ export interface ZFormProps<T extends FieldValues> {
 export function ZForm<T extends FieldValues>(props: ZFormProps<T>) {
   const { instance, submit } = props.form;
   const onSubmit = instance.handleSubmit(submit);
+  const { isSubmitting } = instance.formState;
+
   return (
     <ZFormContext.Provider value={instance}>
       <Form {...instance}>
-        <form onSubmit={onSubmit} className={props.className}>
-          {props.children}
+        <form onSubmit={onSubmit} className="w-full">
+          <fieldset
+            disabled={isSubmitting}
+            className={cn('min-w-0 border-0 p-0 m-0 w-full', props.className)}
+          >
+            {props.children}
+          </fieldset>
         </form>
       </Form>
     </ZFormContext.Provider>
